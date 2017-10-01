220ef17d453fb082318799cb3b0cb067fc93a91a
Ricardo Kim <hexadecimal.exe@gmail.com>
2017-10-01 14:30:21 -0300

    Update Projeto_Base.vhd
    Organization and Commenting the Code

--- MemorySRAM/Projeto_Base.vhd
+++ MemorySRAM/Projeto_Base.vhd
@@ -13,21 +13,13 @@ ENTITY Projeto_Base IS
 		CLOCK_50 : IN bit;
 		
 		-- memoria SRAM
-		SRAM_ADDR 	: 		OUT 		std_logic_vector (17 downto 0);
-		SRAM_DQ 		: 		INOUT 	std_logic_vector (15 downto 0);
-		SRAM_WE_N	: 		OUT 		bit; -- write enable 0
-		SRAM_OE_N	: 		OUT 		bit; -- read enable 0
-		SRAM_UB_N	: 		OUT 		bit; -- Data Mask High-Byte 0
-		SRAM_LB_N	: 		OUT 		bit; -- Data Mask Low-Byte 0
-		SRAM_CE_N	: 		OUT 		bit; -- Chip Enable 0
-		
-		-- memoria FLASH
-		fl_addr		:		out		std_logic_vector (21 downto 0);
-		fl_dq			:		inout		std_logic_vector (7 downto 0);
-		fl_oe_n		:		out		bit; -- read/output Enable in 0
-		fl_rst_n		:		out		bit; -- reset enable in 0
-		fl_we_n		:		out		bit; -- write enable in 0
-		
+		SRAM_ADDR 	: 	OUT 		std_logic_vector (17 downto 0);
+		SRAM_DQ 	: 	INOUT 		std_logic_vector (15 downto 0);
+		SRAM_WE_N	: 	OUT 		bit; -- write enable 0
+		SRAM_OE_N	: 	OUT 		bit; -- read enable 0
+		SRAM_UB_N	: 	OUT 		bit; -- Data Mask High-Byte 0
+		SRAM_LB_N	: 	OUT 		bit; -- Data Mask Low-Byte 0
+		SRAM_CE_N	: 	OUT 		bit; -- Chip Enable 0		
 		
 		-- interface
 		HEX0 : OUT bit_vector (0 to 6);
@@ -38,7 +30,7 @@ ENTITY Projeto_Base IS
 		LEDG : OUT std_logic_vector (7 downto 0);
 		LEDR : OUT std_logic_vector (9 downto 0);
 		
-		SW: IN std_logic_vector (9 downto 0);
+		SW : IN std_logic_vector (9 downto 0);
 		KEY: IN bit_vector (0 to 3)
 		
 	);
@@ -52,6 +44,7 @@ ARCHITECTURE Kim OF Projeto_Base IS
 	SIGNAL button0: bit;
 	
 	-- conversion the address to hex display
+	-- 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
 	TYPE Dec IS ARRAY (0 to 15) OF bit_vector (0 to 6);
 	CONSTANT DB: Dec :=(
 		0 =>  "0000001",
@@ -72,89 +65,72 @@ ARCHITECTURE Kim OF Projeto_Base IS
 		15=>  "0111000"
 		
 	);
-	-- memory SRAM
-	-- Address
-	--signal  		address17Bit	: 	std_logic_vector (17 downto 0) 	:= 	"000000000000000001";	
-	--constant  	oneBit			: 	std_logic_vector (17 downto 0) 	:= 	"000000000000000001";
+	-- memory
 	
 	-- data
-	constant		ioLocker			:	std_logic_vector (15 downto 0)	:=		"ZZZZZZZZZZZZZZZZ";
+	-- High-Z is defined with 'Z' bit
+	constant	ioLocker		:	std_logic_vector (15 downto 0)	:=		"ZZZZZZZZZZZZZZZZ";
 	
 	-- controller
 	signal		writeEnable		: 	bit;
 	signal		readEnable		: 	bit;
 	
-	
-	-- memory flash
 	-- address
-	signal  		address21Bit	: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000000";	
-	constant  	one21Bit			: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000001";
-	constant  	address21Start	: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000000";
+	-- i used this to navigate by the memory addressing
+	signal  	address21Bit		: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000000";	
+	constant  	one21Bit		: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000001";
+	constant  	address21Start		: 	std_logic_vector (21 downto 0) 	:= 	"0000000000000000000000";
 
 	
 	
 	BEGIN	
 		-- INTERFACE
-		LEDR <= SW;	
-					
-		--hex3 <= db(to_integer(ieee.numeric_std.unsigned(address17Bit(15 downto 12))));
-		--hex2 <= db(to_integer(ieee.numeric_std.unsigned(address21Bit(11 downto 8))));
-		hex3 <= db(to_integer(ieee.numeric_std.unsigned(sw(9 downto 9))));
-		hex2 <= db(to_integer(ieee.numeric_std.unsigned(sw(8 downto 8))));
+		LEDR <= SW; -- this allow me a preview what will be saved inside the memory Address
+
+		hex3 <= db(to_integer(ieee.numeric_std.unsigned(sw(9 downto 9)))); -- this allow me to view the Write Enable State
+		hex2 <= db(to_integer(ieee.numeric_std.unsigned(sw(8 downto 8)))); -- this allow me to view the Output Enable State
+		
+		-- here we can check the Address number
+		-- hex3 <= db(to_integer(ieee.numeric_std.unsigned(address17Bit(15 downto 12))));
+		-- hex2 <= db(to_integer(ieee.numeric_std.unsigned(address21Bit(11 downto 8))));
 		hex1 <= db(to_integer(ieee.numeric_std.unsigned(address21Bit(7 downto 4))));
 		hex0 <= db(to_integer(ieee.numeric_std.unsigned(address21Bit(3 downto 0))));
 		
 		-- MEMORY SRAM
 		SRAM_ADDR 		<= 	address21Bit(17 downto 0);
-		writeEnable		<=		to_bit(SW(9));
-		readEnable		<=		to_bit(SW(8));
-		SRAM_CE_N		<=		'0'; -- Chip Enable 0
+		writeEnable		<=	to_bit(SW(9));
+		readEnable		<=	to_bit(SW(8));
+		SRAM_CE_N		<=	'0'; -- Chip Enable 0
 		SRAM_UB_N 		<= 	'0'; -- Data Mask High-Byte 0
 		SRAM_LB_N 		<= 	'0'; -- Data Mask Low-Byte 0
-		
-		--SRAM_OE_N		<=		to_bit(SW(8)); -- read enable 0
-		--SRAM_WE_N   	<=		to_bit(SW(9));
-		
-		--SRAM_WE_N: in bit; -- write enable 0
-		--SRAM_OE_N: in bit; -- read enable 0
-		
-		-- MEMORY FLASH
-		--fl_addr 			<= 	address21Bit;
-		--fl_rst_n 		<= 	'0'; 
-		--fl_dq				<= 	sw(7 downto 0); -- write state at key2
-		--fl_oe_n 			<= 	to_bit(sw(8));
-		--fl_we_n 			<= 	to_bit(sw(9));
-		
 		
 		PROCESS (CLOCK_50)
 			BEGIN
 			IF (CLOCK_50'EVENT and CLOCK_50 = '0')THEN
-				-- Locked the I/O
-				if	writeEnable = '1' and readEnable = '1' then
-				-- =========================
-					SRAM_DQ 			<= 	ioLocker;
-					SRAM_OE_N		<=		'1';
-					SRAM_WE_N   	<=		'1';
-				-- =========================
+				-- Allows Move Address to UP and Start Address
+				if writeEnable = '1' and readEnable = '1' then
+				-- =======================================
+					SRAM_DQ 	<= 	ioLocker; -- Locked the I/O
+					SRAM_OE_N	<=	'1';
+					SRAM_WE_N   	<=	'1';
+				-- =======================================
 					
-					-- Enable Move Address to UP
+					-- Move Address to UP
 					IF (KEY(3) = '0') THEN
 						IF button3 = '0' then
-							--address17Bit <= address17Bit + oneBit;
-							address21Bit <= address21Bit + one21Bit;
-							button3 <= '1';
+							address21Bit 	<= 	address21Bit + one21Bit;
+							button3 	<= 	'1';
 						end if;
 						
 					ELSE
 						button3 <= '0';
 					END IF;
 					
-					-- Enable Move Address to DOWN
+					-- Move to first Address 000000ect
 					IF (KEY(2) = '0') THEN
 						if button2 = '0' then
-							--address17Bit <= oneBit;
-							address21Bit <= address21Start;
-							button2	<= '1';
+							address21Bit 	<= 	address21Start;
+							button2		<= 	'1';
 						end if;
 					ELSE
 						button2 <= '0';
@@ -162,16 +138,16 @@ ARCHITECTURE Kim OF Projeto_Base IS
 				
 				--	read is enabled
 				elsif writeEnable = '1' and readEnable = '0'  then
-				-- =========================
-					SRAM_OE_N		<=		'0';
-					SRAM_WE_N   	<=		'1';
-				-- =========================
+				-- ===================================
+					SRAM_OE_N	<=	'0';
+					SRAM_WE_N   	<=	'1';
+				-- ===================================
 					-- read
 					IF (KEY(0) = '0') THEN
 						if button0 = '0' then
+							-- read 8 bits lower sifnificant from the memory to the Green Leds
+							LEDG 	<= 	SRAM_DQ(7 downto 0); -- 8 bits because hardware limitations
 							button0	<= 	'1';
-							--ledg 		<= 	fl_dq;
-							LEDG 	<= 	SRAM_DQ(7 downto 0);
 						end if;
 					ELSE
 						button0 <= '0';
@@ -179,16 +155,16 @@ ARCHITECTURE Kim OF Projeto_Base IS
 		
 				-- write is enabled
 				elsif writeEnable = '0' and readEnable = '0'  then
-				-- =========================
-					SRAM_OE_N		<=		'0';
-					SRAM_WE_N   	<=		'0';
-				-- =========================
+				-- =====================================
+					SRAM_OE_N	<=	'0';
+					SRAM_WE_N   	<=	'0';
+				-- =====================================
 					-- write
 					IF (KEY(1) = '0') THEN
 						if button1 = '0' then
-							button1		<= 	'1';
+							-- Rec the 18 bits in the memory data
 							SRAM_DQ 	<= 	("000000"&SW(9 downto 0));
-							--fl_dq 		<= 	(sw(7 downto 0));
+							button1		<= 	'1';
 						end if;
 					ELSE
 						button1 <= '0';
